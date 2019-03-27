144c4a1ad413b136a251dc9b3d22ab667241a95d
Adam Crampton <adam.crampton@alluremedia.com.au>
2019-03-28 10:13:59 +1100

    Fixed request and hash strings. Outputting playlist name in each iteration. Problem with timestamp though, response says it's in the future

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ const request = require('request');
 
 // Define settings
 // ===============
-const playlistNames = ['array', 'of','your', 'playlist', 'names'];
+const playlistNames = ['your', 'array', 'of', 'playlist', 'names'];
 const endpoint = "https://api.jwplatform.com/v1/channels/create?text=playlistcreation&type=article_matching";
 
 // Credentials
 // ===========
-const api_key = "yourkey";
+const api_key = "yourapikey";
 const api_secret = "yoursecret";
+const api_timestamp = Date.now();
 const api_nonce = makeNonce(8);
 const api_signature = makeApiSignature();
 
@@ -25,9 +26,10 @@ const playlistLength = playlistNames.length;
 
 for (let i=0; i < playlistLength; i++) {
   // Create request string.
-  let requestString = endpoint + '&title=' + playlistNames[i] + '&' + api_signature;
+  let requestString = endpoint + '&title=' + playlistNames[i] + '&api_format=xml' + '&api_key=' + api_key + '&api_nonce=' + api_nonce + '&api_timestamp=' + api_timestamp + '&api_signature=' + api_signature;
 
   request(requestString, function (error, response, body) {
+    console.log('Processing creation of playlist: ' + playlistNames[i])
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Output response body
@@ -50,7 +52,7 @@ function makeNonce(length) {
 
 function makeApiSignature() {
   // Create string of parameters to be hashed.
-  let hashString = 'api_format=xml&api_key=' + api_key + '&api_nonce=' + api_nonce + '&api_timestamp=' + Date.now() + '&text=playlistcreation' + api_secret;
+  let hashString = 'api_format=xml&api_key=' + api_key + '&api_nonce=' + api_nonce + '&api_timestamp=' + api_timestamp + '&text=playlistcreation' + api_secret;
 
     shaSum = crypto.createHash('sha1');
     shaSum.update(hashString);
